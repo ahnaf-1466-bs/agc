2a839a7abd996757d93c93034d00088639464835
ahnaf-1466-bs <ahnaf.mubashshir@brainstation-23.com>
2024-11-14 18:33:02 +0600

    moodle deleted

--- theme/edumy/layout/ccn_minimal.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-include($CFG->dirroot . '/theme/edumy/ccn/ccn_themehandler.php');
-array_push($extraclasses, "ccn_context_dashboard");
-$bodyclasses = implode(" ",$extraclasses);
-$bodyattributes = $OUTPUT->body_attributes($bodyclasses);
-include($CFG->dirroot . '/theme/edumy/ccn/ccn_themehandler_context.php');
-echo $OUTPUT->render_from_template('theme_edumy/ccn_minimal', $templatecontext);
